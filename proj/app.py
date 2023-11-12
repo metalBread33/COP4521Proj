c71717fbb2048b410933eccdb5cc533a2564c7f6
metalBread33 <kfoughty80@gmail.com>
2023-11-12 19:31:28 +0000

    Start of git repo for cop4521 project

---
+++ proj/app.py
@@ -0,0 +1,39 @@
+from flask import Flask, render_template, jsonify
+import sqlite3
+import requests
+
+app = Flask(__name__)
+
+@app.route("/")
+@app.route("/home")
+def home(): 
+    return "Hello"
+
+@app.route("/newsfeed")
+def newsfeed():
+    con = sqlite3.connect("hackernews_data.db")
+    cursor = con.cursor()
+    news = " SELECT * FROM hackernews_data ORDER BY time DESC LIMIT 30"
+    cursor.execute(news)
+    items = cursor.fetchall()
+    item_fields = ['id', 'by', 'descendants', 'kids', 'score', 'text', 'time', 'title', 'type']
+    data = [dict(zip(item_fields, news)) for news in items]
+    con.close()
+    return jsonify(data)
+
+@app.route("/admin")
+def admin():
+    return "Admin page"
+
+@app.route("/login")
+def login():
+    return "Login page"
+
+@app.route("/signup")
+def signup():
+    return "Sign up page"
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ proj/makeTable.py
@@ -0,0 +1,29 @@
+import sqlite3
+
+# Connect to the SQLite database (or create it if it doesn't exist)
+conn = sqlite3.connect("hackernews_data.db")
+cursor = conn.cursor()
+
+# Define the SQL command to create the table
+create_table_sql = """
+CREATE TABLE IF NOT EXISTS hackernews_data (
+    id INTEGER PRIMARY KEY,
+    by TEXT,
+    descendants INTEGER,
+    kids BLOB,
+    score INTEGER,
+    text TEXT,
+    time INTEGER,
+    title TEXT,
+    type TEXT,
+    url TEXT
+);
+"""
+
+# Execute the SQL command to create the table
+cursor.execute(create_table_sql)
+
+# Commit the changes and close the connection
+conn.commit()
+conn.close()
+
